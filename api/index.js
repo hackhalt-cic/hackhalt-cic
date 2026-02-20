c7bd4e239ca95d318b9b7b11e72dd46007282a4f
Blood Bank Developer <dev@bloodbank.com>
2026-02-20 12:21:51 +0530

    Fix Vercel routing

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-// Vercel serverless entry for Express app
 const serverless = require('serverless-http');
-require('dotenv').config();
+const app = require('../server');
 
-// Import the Express app directly (not wrapped)
-const app = require('../server.js');
-
-// Wrap Express app for Vercel serverless
 module.exports = serverless(app);
